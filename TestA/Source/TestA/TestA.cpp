15d5c3086c747d90529e0f3719af386d81f366ea
Maximilian Nasert <maximian.nasert@gmx.de>
2021-08-27 14:03:07 +0200

    delete

--- TestA/Source/TestA/TestA.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TestA.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestA, "TestA" );
